de48e9ff16171b92496ab9701f90cbc15446a5a9
Jeff Prestes <jeffprestes@gmail.com>
2024-12-03 18:04:10 -0300

    feat: static data

--- test/ParedesNFT.t.sol
+++ test/ParedesNFT.t.sol
@@ -6,9 +6,28 @@ import {ParedesNFT} from "../src/ParedesNFT.sol";
 
 contract ParedesNFTTest is Test {
     ParedesNFT public paredes;
+    address public owner = 0x1234567890123456789012345678901234567890;
 
     function setUp() public {
-        paredes = new ParedesNFT(msg.sender);
+        paredes = new ParedesNFT(owner);
     }
 
+    function test_mint() public {
+        uint256 id = 1;
+        uint256 amount = 1;
+        bytes memory data = "0x";
+        vm.prank(owner);
+        paredes.mint(owner, id, amount, data);
+        uint256 balance = paredes.balanceOf(owner, id);
+        assertEq(balance, amount);
+    }
+
+    function test_uri() public {
+        uint256 id = 1;
+        string memory uri = "https://example.com";
+        vm.prank(owner);
+        paredes.setURI(id, uri);
+        string memory actual = paredes.uri(id);
+        assertEq(actual, uri);
+    }
 }
